2c2f51c74a2d836e480d94537ba499e21ecd8b19
Hemanth1992patil <hemanthareddy.p@acldigital.com>
2024-02-17 18:40:23 +0530

    changed files stored now in server not in  db

--- consumer/view.py
+++ consumer/view.py
@@ -3,15 +3,17 @@ from ceq_user.resources.errors import unauthorized
 from flask import request, jsonify
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from werkzeug.utils import secure_filename
 from mongoengine import DoesNotExist
 from bson import ObjectId
 import json
 from datetime import datetime
 import base64
+import os
 import io
+import uuid
 
-# fs = GridFS(connect(db='ceq', host='localhost'))
-# db = client[your_database]  # Replace 'your_database' with your actual database name
+UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'C:\\Users\\hemanthareddy.p\\Desktop\\ceq_main\\static\\consumer')
 
 
 # This api is used to return hello world
@@ -36,6 +38,18 @@ class DeleteConsumerAudit(Resource):
             if audit_document is None:
                 return jsonify({'message': 'Audit ID Not Found'})
             if audit_document:
+                audit_json = json.loads(audit_document.to_json())
+                for key in ['audit_signature', 'audited_staff_signature']:
+                    if key in audit_json:
+                        if os.path.exists(audit_json[key]):
+                            os.remove(audit_json[key])      
+                if "ceqvs" in audit_json:
+                    for img in audit_json["ceqvs"]:
+                        if "image" in audit_json["ceqvs"][img]:
+                            if os.path.exists(audit_json["ceqvs"][img]["image"]):
+                                print(audit_json["ceqvs"][img]["image"])
+                                os.remove(audit_json["ceqvs"][img]["image"])
+          
                 audit_document.delete()
                 return jsonify({"message": "Audit with ID deleted successfully"})
             else:
@@ -51,29 +65,29 @@ class GetConsumerAudit(Resource):
         try:
             user = User.objects.get(id=get_jwt_identity()['id'])
         except DoesNotExist:
-            return unauthorized()  
-        
+            return unauthorized()
         if user.role != "audit" and user.permission != "consumer":
             return jsonify({"message" : "'error': 'Unauthorized access'"})
-              
-        
         try:            
             audit_id = ObjectId(request.args.get('audit_id'))
             audit_data = AuditData.objects(id=audit_id).first()
+            if audit_data is None:
+                return jsonify({"message" : "Audit Id Not Found"})
             if audit_data:
-                # Serialize audit_data to JSON
                 audit_json = json.loads(audit_data.to_json())
-                # Convert audit_signature and audited_staff_signature to base64 encoded strings
-                for signature_key in ['audit_signature', 'audited_staff_signature']:
-                    if hasattr(audit_data, signature_key):
-                        signature_stream = getattr(audit_data, signature_key)
-                        if signature_stream:
-                            audit_json[signature_key] = base64.b64encode(signature_stream.read()).decode('utf-8')
-            
-                return jsonify(audit_json)
-            else:
-                return jsonify({'message': 'Audit not found'})
-
+                for key in ['audit_signature', 'audited_staff_signature']:
+                    if key in audit_json:
+                        if os.path.exists(audit_json[key]):
+                            with open(audit_json[key], 'rb') as file:
+                                audit_json[key] = base64.b64encode(file.read()).decode('utf-8')         
+                if "ceqvs" in audit_json:
+                    for img in audit_json["ceqvs"]:
+                        print(audit_json["ceqvs"][img])
+                        if "image" in audit_json["ceqvs"][img]:
+                            if os.path.exists(audit_json["ceqvs"][img]["image"]):
+                                with open(audit_json["ceqvs"][img]["image"], 'rb') as file:
+                                    audit_json["ceqvs"][img]["image"] = base64.b64encode(file.read()).decode('utf-8')
+            return jsonify(audit_json)   
         except Exception as e:
             print("Exception: ", e)
             return jsonify({'message': 'Error occurred while retrieving audit'})
@@ -90,23 +104,26 @@ class GetConsumerAuditList(Resource):
             return jsonify({"message" : "'error': 'Unauthorized access'"})        
         
         try:
-            print("Dateeeeeeeeeeeeeeeeee",datetime.now())
             # Retrieve all audit data
             all_audit_data = AuditData.objects()
-
             if all_audit_data:
                 # Initialize an empty list to store serialized audit data
                 audit_list = []
                 # Iterate through each audit and serialize it to JSON
                 for audit_data in all_audit_data:
                     audit_json = json.loads(audit_data.to_json())
-                    # Convert audit_signature and audited_staff_signature to base64 encoded strings
-                    for signature_key in ['audit_signature', 'audited_staff_signature']:
-                        if hasattr(audit_data, signature_key):
-                            signature_stream = getattr(audit_data, signature_key)
-                            if signature_stream:
-                                audit_json[signature_key] = base64.b64encode(signature_stream.read()).decode('utf-8')
-                    # Append the serialized audit data to the audit_list
+                    for key in ['audit_signature', 'audited_staff_signature']:
+                        if key in audit_json:
+                            if os.path.exists(audit_json[key]):
+                                with open(audit_json[key], 'rb') as file:
+                                    audit_json[key] = base64.b64encode(file.read()).decode('utf-8')         
+                    if "ceqvs" in audit_json:
+                        for img in audit_json["ceqvs"]:
+                            print(audit_json["ceqvs"][img])
+                            if "image" in audit_json["ceqvs"][img]:
+                                if os.path.exists(audit_json["ceqvs"][img]["image"]):
+                                    with open(audit_json["ceqvs"][img]["image"], 'rb') as file:
+                                        audit_json["ceqvs"][img]["image"] = base64.b64encode(file.read()).decode('utf-8')
                     audit_list.append(audit_json)
                 return jsonify(audit_list)
             else:
@@ -116,83 +133,6 @@ class GetConsumerAuditList(Resource):
             print("Exception: ", e)
             return jsonify({'message': 'Error occurred while retrieving audits'})
 
-class UpdateConsumerAudit(Resource):
-    @jwt_required()
-    def post(self):
-        try:
-            user = User.objects.get(id=get_jwt_identity()['id'])
-        except DoesNotExist:
-            return unauthorized()       
-        if (user.role == "audit" or user.role == "superviser" )  and (user.permission != "consumer"):
-            return jsonify({"message" : "'error': 'Unauthorized access'"})
-           
-        try:
-            # Retrieve the audit data by audit_id
-            id = ObjectId(request.args.get('audit_id'))            
-            audit_data = AuditData.objects(id=id).first()
-            if audit_data is None:
-                return jsonify({'message': 'Audit ID Not Found'})
-            
-            if audit_data:
-                # Access form data
-                data = request.form
-                # Update Form1 object
-                form1_data = json.loads(data.get('form1'))
-                form1 = Form1(
-                    supervisor_contact=form1_data.get('supervisor_contact'),
-                    tech_pt=form1_data.get('tech_pt'),
-                    vehicle_number=form1_data.get('vehicle_number'),
-                    tech_skills=form1_data.get('tech_skills'),
-                    sr_manager=form1_data.get('sr_manager'),
-                    tech_fullname=form1_data.get('tech_fullname'),
-                    region=form1_data.get('region'),
-                    vendor=form1_data.get('vendor'),
-                    director=form1_data.get('director'),
-                    sr_number=form1_data.get('sr_number'),
-                    tech_ein=form1_data.get('tech_ein'),
-                    team=form1_data.get('team'),
-                    duty_manager=form1_data.get('duty_manager'),
-                    supervisor=form1_data.get('supervisor'),
-                    shortdescription=form1_data.get('shortdescription'),
-                    tech_contact=form1_data.get('tech_contact')
-                )
-                audit_data.form1 = form1
-
-                form2_data_str = data.get('ceqvs')
-                form2_data = json.loads(form2_data_str)
-                form2_objects = {}
-                for key, value in form2_data.items():
-                    form2_objects[key] = Form2(**value)
-                 
-                audit_data.ceqvs = form2_objects
-                # Update other audit data fields
-                audit_data.status = data.get('status')
-                audit_data.name = data.get('name')
-                audit_data.lastmodified = datetime.now()
-                audit_data.supervisor_id = data.get('supervisor_id')
-                audit_data.expiryDate = data.get('expiryDate')
-                audit_data.auditDate = data.get('auditDate')
-                audit_data.remarks = data.get('remarks')
-                audit_data.department = data.get('department')
-                audit_data.description = data.get('description')
-                # Update images
-                for image_key, file_data in request.files.items(): 
-                    if image_key in ['audit_signature', 'audited_staff_signature']:
-                        signature_stream = io.BytesIO(file_data.read())
-                        setattr(audit_data, image_key, signature_stream) 
-
-                # Save the updated audit data
-                audit_data.save()
-
-                return jsonify({'message': 'Audit updated successfully'})
-            else:
-                return jsonify({'message': 'Audit not found'})
-
-        except Exception as e:
-            print("Exception: ", e)
-            return jsonify({'message': 'Error occurred while updating audit'})
-
-
 class CreateConsumerAudit(Resource):
     @jwt_required()
     def post(self):
@@ -201,7 +141,7 @@ class CreateConsumerAudit(Resource):
         except DoesNotExist:
             return unauthorized()
         if user.role != "audit" and user.permission != "consumer":
-            return jsonify({"message": "Unauthorized access"})
+            return jsonify({"message": "Unauthorized access"})        
         try:
             # Access form data
             data = request.form
@@ -211,9 +151,7 @@ class CreateConsumerAudit(Resource):
             # Create Form2 objects
             form2_data_str = data.get('ceqvs')
             form2_data = json.loads(form2_data_str)
-            form2_objects = {}
-            for key, value in form2_data.items():
-                form2_objects[key] = Form2(**value)
+            form2_objects = {} 
             # Create Form1 object
             form1 = Form1(**form1_data)
             # Create AuditData object
@@ -225,21 +163,142 @@ class CreateConsumerAudit(Resource):
                 expiryDate=datetime.now(),
                 auditDate=data.get('auditDate'),
                 remarks=data.get('remarks'),
-                ceqvs=form2_objects,
                 department=data.get('department'),
                 createdDate=data.get('createdDate'),
                 description=data.get('description')
             )  
             
+            ceqv_images = []
+            ceq_obj = []
+            for obj in form2_data:
+                ceq_obj.append(str(obj))
+                if "image" in form2_data[obj]:
+                    ceqv_images.append(form2_data[obj]['image'])      
             for image_key, file_data in request.files.items():
-                if image_key in ['audit_signature', 'audited_staff_signature']:
-                    signature_stream = io.BytesIO(file_data.read())
-                    setattr(audit_data, image_key, signature_stream) 
-            # Save the audit data to the database
+                if image_key in ['audited_staff_signature', 'audit_signature']:
+                    # Generate a unique filename
+                    unique_filename = str(uuid.uuid4()) + '_' + secure_filename(file_data.filename)
+                    file_path = os.path.join(UPLOAD_FOLDER, unique_filename)
+                    if image_key == 'audited_staff_signature':
+                        audit_data.audited_staff_signature = file_path
+                    if image_key == 'audit_signature':
+                        audit_data.audit_signature = file_path
+                    file_data.save(file_path)
+                if image_key in ceqv_images:
+                    unique_filename = str(uuid.uuid4()) + '_' + secure_filename(file_data.filename)
+                    file_path = os.path.join(UPLOAD_FOLDER, unique_filename)
+                    for i in ceq_obj:
+                        if "image" in form2_data[i]:
+                            if form2_data[i]['image'] == image_key:
+                                form2_data[i]['image'] = file_path
+                                file_data.save(file_path)
+            
+            for key, value in form2_data.items():
+                form2_objects[key] = Form2(**value)         
+            audit_data.ceqvs = form2_objects
             audit_data.save()
 
             return jsonify({'message': 'Audit created successfully'})
 
         except Exception as e:
             print("Exception:", e)
-            return jsonify({'message': 'Error occurred while creating audit'})
+            return jsonify({'message': "error:{}".format(e)})
+        
+class UpdateConsumerAudit(Resource):
+    @jwt_required()
+    def post(self):
+        try:
+            user = User.objects.get(id=get_jwt_identity()['id'])
+        except DoesNotExist:
+            return unauthorized()
+        if user.role != "audit" and user.permission != "consumer":
+            return jsonify({"message": "Unauthorized access"})
+        
+        audit_id = ObjectId(request.args.get("audit_id"))
+        audit_data = AuditData.objects(id=audit_id).first()
+        image_file = request.files
+        if audit_data is None:
+            print({"message": "Audit not found"})
+            return jsonify({"message": "Audit not found"})  
+        try:
+            if audit_data:
+                file_remove = []
+                audit_json = json.loads(audit_data.to_json())
+                for key in ['audit_signature', 'audited_staff_signature']:
+                    if key in audit_json:
+                        file_remove.append(audit_json[key])                      
+                if "ceqvs" in audit_json:
+                    for obj in audit_json['ceqvs']:       
+                        if "image" in audit_json['ceqvs'][obj]:
+                            file_remove.append(audit_json['ceqvs'][obj]["image"])
+                print("file_remove ", file_remove)
+                for fr in file_remove:
+                    if os.path.exists(fr):
+                        os.remove(fr)
+                # Access form data
+                data = request.form
+                form1_data = data.get('form1')
+                # Convert JSON strings to Python dictionaries
+                form1_data = json.loads(form1_data)
+                # Create Form1 object
+                form1 = Form1(**form1_data)
+                # Update Form1 data
+                for key, value in form1_data.items():
+                    setattr(audit_data.form1, key, value)
+                # Update other audit data fields
+                audit_data.status = data.get('status')
+                audit_data.lastmodified = datetime.now()
+                audit_data.supervisor_id = int(data.get('supervisor_id'))
+                audit_data.expiryDate = datetime.now()  # Update as needed
+                audit_data.auditDate = data.get('auditDate')
+                audit_data.remarks = data.get('remarks')
+                audit_data.department = data.get('department')
+                audit_data.createdDate = data.get('createdDate')
+                audit_data.description = data.get('description')
+                # Update CEQVs
+                form2_data_str = data.get('ceqvs')
+                form2_data = json.loads(form2_data_str)
+                form2_objects = {}
+                for key, value in form2_data.items():
+                    form2_objects[key] = Form2(**value)
+                ceqv_images = []
+                ceq_obj = []
+                for obj in form2_data:
+                    ceq_obj.append(str(obj))
+                    if "image" in form2_data[obj]:
+                        ceqv_images.append(form2_data[obj]['image'])
+                # Update images
+                
+                for image_key, file_data in image_file.items():
+                    if image_key in ['audited_staff_signature', 'audit_signature']:
+                        # Generate a unique filename
+                        unique_filename = str(uuid.uuid4()) + '_' + file_data.filename
+                        file_path = os.path.join(UPLOAD_FOLDER, unique_filename)
+                        if image_key == 'audited_staff_signature':
+                            if os.path.exists(audit_data.audited_staff_signature):
+                                file_remove.append(audit_data.audited_staff_signature)                                   
+                            audit_data.audited_staff_signature = file_path
+                            
+                        if image_key == 'audit_signature':
+                            audit_data.audit_signature = file_path
+                        file_data.save(file_path)
+                    if image_key in ceqv_images:
+                        unique_filename = str(uuid.uuid4()) + '_' + file_data.filename
+                        file_path = os.path.join(UPLOAD_FOLDER, unique_filename)
+                        for i in ceq_obj:
+                            if "image" in form2_data[i]:
+                                if form2_data[i]['image'] == image_key:                                    
+                                    form2_data[i]['image'] = file_path
+                                    file_data.save(file_path) 
+                for key, value in form2_data.items():
+                    form2_objects[key] = Form2(**value)                        
+                audit_data.ceqvs = form2_objects
+                # Save the updated audit data
+                audit_data.save()
+
+                return jsonify({'message': 'Audit updated successfully'})
+        except Exception as e:
+            print("Exception:", e)
+            return jsonify({'message':'Error occurred while retrieving audits'})
+        
+        
